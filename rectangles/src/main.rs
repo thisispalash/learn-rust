ee1616a6e791306b68007681f309c921c340c74a
Palash A <palash.agarwal.96@gmail.com>
2022-01-21 20:07:35 +0530

    with tuples

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -6,8 +6,20 @@ fn main() {
         "The area of the rectangle is {} square pixels.",
         area(width1, height1)
     );
+
+
+    let rect1 = (30,50);
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        area_tup(rect1)
+    );
 }
 
 fn area(width: u32, height: u32) -> u32 {
     width * height
+}
+
+fn area_tup(dimensions: (u32, u32)) -> u32 {
+    dimensions.0 * dimensions.1
 }
